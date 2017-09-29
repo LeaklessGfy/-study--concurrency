9c698b74ea6d8f2f6803e49c80c6c861d0b76801
Leakless <biokille77@gmail.com>
2017-09-29 21:53:27 +0200

    Improve answer

--- TP1/src/HelloListBug.java
+++ TP1/src/HelloListBug.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
  * Exercice 3
  * @author Vincent Rasquier
  *
- * @Q2 Car dans la methode add, le champs size est mise à jour. Entre le temps de lecture de la valeur size et le moment d'écriture de la nouvelle valeur, l'objet list peut avoir évolué à cause d'un autre thread. 
- * @Q3 Il peut y avoir un out-of-bounds, c'est à dire que la size devient supérieur à la taille du tableau et donc ne passe pas dans le grow. De plus, avec les optimisations de Java, il se peut que le processeur change l'ordre d'execution de certaine instruction comme 'augmenter la taille du tableau' avant de finir la copie entière du tableau.
+ * @Q2 La taille de la list est plus petite que le nombre total d"élément car dans la method add, le champs size est mise à jour. Ce champs représente la dernière case de la list qui est disponnible (et ainsi la taille de celle-ci). Entre le temps de lecture de la valeur size (au moment du size++) et le moment d'écriture de la nouvelle valeur, l'objet list peut avoir évolué à cause d'un autre thread (+1, +2, +*) or au retour sur le thread avant interruption, le thread ne prendra pas en compte ces ajouts.
+ * @Q3 Il peut y avoir un out-of-bounds, c'est à dire que la size devient supérieur à la taille du tableau et donc ne passe plus dans le grow car la method add fait un test d'égalité et non >. De plus, avec les optimisations de Java, il se peut que le processeur change l'ordre d'execution de certaine instruction comme 'augmenter la taille du tableau' avant de finir la copie entière du tableau (array.copy).
  */
 public class HelloListBug {
 	private static final int NB_THREADS = 4;
--- TP1/src/HelloThread.java
+++ TP1/src/HelloThread.java
@@ -2,8 +2,9 @@
  * Exercice 1
  * @author Vincent Rasquier
  *
- * @Q1
- * @Q3
+ * @Q1 Un Runnable est une interface fonctionnelle qui ne prend pas d'argument et qui retourne void. Cette interface sert, dans notre cas, à executer du code dans un thread.
+ * @Q3 Nous remarquons que l'affichage se fait par période. En effet, l'affichage produit est une suite croissante de nombre pendant une période T non constante. Cette suite s'interrompt avec une autre suite après la période T.
+ * Ce comportement est normal car System.out.println est Thread safe. En effet, en observant le code de la method nous observons qu'un synchornized est présent.
  */
 public class HelloThread {
 	private static final int NB_THREADS = 4;
